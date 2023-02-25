749873c2269b50a8d8489e4024a6a6a409f1b9fd
Eduardo Salgado <edusalgado00@gmail.com>
2023-02-25 17:11:46 -0600

    Cambios en el CSS y algunas lineas del login

--- markeplace-frontend/src/component/sig-in/login.js
+++ markeplace-frontend/src/component/sig-in/login.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
+
 function Login (){
     
     return(
         
         <div className='container'>
-            <div className='card'>
                 <div className='card-body'>
                     <form>      
-                        <h5>Ingrese su nombre de usuario y contraseña</h5>
+                        <h2>Iniciar Sesion</h2>
+                        <br></br>
                         <div className='mb-3'>
                             <label className='form-label'>Nombre de usuario</label>
                             <input className='form-control' name='name-user' type='emailHelp' />
@@ -17,13 +18,13 @@ function Login (){
                             <label className='form-label' >Contraseña</label>
                             <input className='form-control' name='pass-user' type='password'/>
                         </div>
-                        <div className='mb-3'>
+                        <br></br>
+                        <div className='mb-2'>
                             <button className='btn btn-primary' type='button' >Enviar</button>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
+                 </div>
+        </div> 
     );
   
 }
