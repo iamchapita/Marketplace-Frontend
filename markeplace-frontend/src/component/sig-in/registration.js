cc9d2efcdaaf4e5e89e7e3b51f1860333c707f5c
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2023-02-25 17:59:22 -0600

    se reorganizo algunos elementos.

--- markeplace-frontend/src/component/sig-in/registration.js
+++ markeplace-frontend/src/component/sig-in/registration.js
@@ -23,51 +23,34 @@ function RegistrationPage() {
               <h2>Registro</h2>
               <form onSubmit={handleSubmit}>
                 <div className='mb-3'>
-                <label className='form-label'>
-                    Nombre completo:
+                    <label className='form-label'>Nombre completo</label>
                     <input className='form-control' type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                  </label>
-                  <br />
                   </div>
                   <div className='mb-3'>
-                    <label>
-                      Correo electronico:
+                      <label className='form-label'>Correo electronico</label>
                       <input className='form-control' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </label>
-                    <br />
                   </div>
                   <div className='mb-3'>
-                    <label className='form-label'>
-                      DNI:
+                      <label className='form-label'>DNI</label>
                       <input className='form-control' type="text" value={identification} onChange={(e) => setIdentification(e.target.value)} />
-                    </label>
-                    <br />
                   </div>
-                  <label className='form-label'>
-                    Numero:
+                  <div className='mb-3'>
+                    <label className='form-label'>Número de teléfono</label>
                     <input className='form-control' type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
-                  </label>
-                  <br />
-                  
-                  <label className='form-label'>
-                    Direccion:
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Dirección</label>
                     <input className='form-control' type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
-                  </label>
-                  <br />
-
-                  <label className='form-label'>
-                    Contraseña:
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Contraseña</label>
                     <input className='form-control' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                  </label>
-                  <br />
-
-                  <label className='form-label'>
-                    Fecha de Nacimiento:
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Fecha de Nacimiento</label>
                     <input className='form-control' type="date" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} />
-                  </label>
-                  <br />
-                  <label></label>
-                  <button className='btn btn-primary' type="submit">Registrarse</button>
+                  </div>
+                    <button className='btn btn-primary' type="submit">Registrarse</button>
                 </form>
           </div>
         </div>
