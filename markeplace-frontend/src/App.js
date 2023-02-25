ddce876593701e52e1d2a4fe64ab8aad4daa0d85
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2023-02-24 21:04:19 -0600

    plantilla proporcionada por react
    la plantilla proporcionada por react, eliminamos el codigo generico que se agrega en la pantalla principal (app.js)

---
+++ markeplace-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './component/login';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Login></Login>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ markeplace-frontend/src/component/login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Login (){
+    /*
+    return(
+        <div className='container'>
+            <form>
+                <div className='container'> 
+                    <h5>Ingrese su nombre de usuario y contraseña</h5>
+                    <div className='mb-3'>
+                        <label className='form-label'>Nombre de usuario</label>
+                        <input className='form-control' name='name-user' type='emailHelp' ></input>
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label' >Contraseña</label>
+                        <input className='form-control' name='pass-user' type='password'></input>
+                    </div>
+                    <div className='mb-3'>
+                        <input className='btn btn-primary' type='button'>Enviar</input>
+                    </div>
+                </div>
+            </form>
+            
+        </div>
+    );
+    */
+   <div></div>
+}
+export default Login;
