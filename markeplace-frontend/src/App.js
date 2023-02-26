b494b7192a30db4b516a3cff4508728b35adc1cb
iamchapita <alejandrom646@gmail.com>
2023-02-26 13:18:46 -0600

    Implementacion de vista login

--- markeplace-frontend/src/App.js
+++ markeplace-frontend/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import Login from './components/auth/login';
 import RegistrationPage from './components/auth/registration.js';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Categories from './components/categories';
 import Navbar from './components/bar/navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const login = () => {
+        setLoggedIn(true);
+    }
+
     return (
         <div className="App">
             <Navbar></Navbar>
             <header className="App-header">
                 <BrowserRouter>
                     <Routes>
-                        <Route path='/login' element={<Login />} ></Route>
+                        <Route path='/login' element={<Login login={login} />} ></Route>
                         <Route path='/registro' element={<RegistrationPage />} ></Route>
                     </Routes>
 
--- markeplace-frontend/src/components/auth/login.js
+++ markeplace-frontend/src/components/auth/login.js
@@ -1,41 +1,40 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import apiClient from '../../services/apiClient';
 
-function Login() {
+const Login = ({ login }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post('http://localhost:8000/api/login',{
+        apiClient.post('api/login', {
             email: email,
             password: password
         }).then((response) => {
             console.log(response);
-        });
-    }
+        })
+    };
 
     return (
-
         <div className='container'>
             <div className='card'>
-            <div className='card-body'>
-                <form onSubmit={handleSubmit}>
-                    <h2>Iniciar Sesion</h2>
-                    <div className='mb-3'>
-                        <label className='form-label'>Correo Electrónico</label>
-                        <input className='form-control' name='email' type='emailHelp' value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className='mb-3'>
-                        <label className='form-label' >Contraseña</label>
-                        <input className='form-control' name='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <div className='mb-2'>
-                        <button className='btn btn-primary' type='submit' >Iniciar Sesión</button>
-                    </div>
-                </form>
-            </div>
+                <div className='card-body'>
+                    <form onSubmit={handleSubmit}>
+                        <h2>Iniciar Sesion</h2>
+                        <div className='mb-3'>
+                            <label className='form-label'>Correo Electrónico</label>
+                            <input className='form-control' name='email' type='emailHelp' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div className='mb-3'>
+                            <label className='form-label' >Contraseña</label>
+                            <input className='form-control' name='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div className='mb-2'>
+                            <button className='btn btn-primary' type='submit' >Iniciar Sesión</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
