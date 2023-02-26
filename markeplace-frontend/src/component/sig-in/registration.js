813a4f171058a2045c0b06a317c3833f3bd5d3fb
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2023-02-25 23:16:14 -0600

    .

--- markeplace-frontend/src/component/sig-in/registration.js
+++ markeplace-frontend/src/component/sig-in/registration.js
@@ -16,6 +16,7 @@ function RegistrationPage() {
         // Handle login logic
     };
 
+<<<<<<< Updated upstream
     return (
         <div>
             <div className='card'>
@@ -57,6 +58,49 @@ function RegistrationPage() {
         </div>
 
     );
+=======
+  return (
+    <div>
+          <div className='card'>
+            <div className='card-body' >
+              <h2>Registro</h2>
+              <form onSubmit={handleSubmit}>
+                <div className='mb-3'>
+                    <label className='form-label'>Nombre completo</label>
+                    <input className='form-control' type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                  </div>
+                  <div className='mb-3'>
+                      <label className='form-label'>Correo electronico</label>
+                      <input className='form-control' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  </div>
+                  <div className='mb-3'>
+                      <label className='form-label'>DNI</label>
+                      <input className='form-control' type="text" value={identification} onChange={(e) => setIdentification(e.target.value)} />
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Número de teléfono</label>
+                    <input className='form-control' type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Dirección</label>
+                    <input className='form-control' type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Contraseña</label>
+                    <input className='form-control' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Fecha de Nacimiento</label>
+                    <input className='form-control' type="date" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} />
+                  </div>
+                    <button className='btn btn-primary' type="submit">Registrarse</button>
+                </form>
+            </div>
+          </div>
+    </div>
+    
+  );
+>>>>>>> Stashed changes
 }
 
 export default RegistrationPage;
