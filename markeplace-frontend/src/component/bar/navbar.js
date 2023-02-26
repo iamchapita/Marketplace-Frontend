9feffd6565becebd05d24b57c6d0a29b0122a70c
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2023-02-25 23:31:07 -0600

    .

--- markeplace-frontend/src/component/bar/navbar.js
+++ markeplace-frontend/src/component/bar/navbar.js
@@ -23,7 +23,7 @@ function Navbar(){
                 </ul>
                 </div>
                 <form className="d-flex" role="search">
-                    <a className="navbar-brand" href="/" >Iniciar Sesión</a>
+                    <a className="navbar-brand" href="/registro" >Registrarse</a>
                     <input className="form-control me-2" type="search" placeholder="Escriba su Busqueda"/>
                     <button className="btn btn-outline-success" type="submit">Buscar</button>
                 </form>
