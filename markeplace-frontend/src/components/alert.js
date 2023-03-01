98e593aa9aa2fe2af25886d924006e4cc14c63d9
iamchapita <alejandrom646@gmail.com>
2023-02-28 20:47:10 -0600

    Correcion : cambio de class por className

--- markeplace-frontend/src/components/alert.js
+++ markeplace-frontend/src/components/alert.js
@@ -6,7 +6,7 @@ const Alert = ({ text, showAlert, setShowAlert }) => {
         return (
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                 <strong>Ups!</strong> {text}
-                <button type="button" class="btn-close" onClick={() => setShowAlert(false)} data-bs-dismiss="alert" aria-label="Close"></button>
+                <button type="button" className="btn-close" onClick={() => setShowAlert(false)} data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         );
     } else {
