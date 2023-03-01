4f59f8501635d5a963fa6146a407b0c043e48773
iamchapita <alejandrom646@gmail.com>
2023-02-28 18:05:30 -0600

    Implementacion : manejo de errores en el login

--- markeplace-frontend/src/components/auth/login.js
+++ markeplace-frontend/src/components/auth/login.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-//import axios from 'axios';
 import apiClient from '../../services/apiClient';
 import '../../style/style-login.css'
 
@@ -15,31 +14,33 @@ const Login = ({ login }) => {
             email: email,
             password: password
         }).then((response) => {
-            console.log(response);
-        })
+            console.log(response.data.message);
+        }).catch((error) => {
+            console.log(error.response.data.message);
+        });
     };
 
     return (
         <div className='box-login'>
             <div className='card-body-login'>
-                    <form onSubmit={handleSubmit}>
-                        <h2>Iniciar Sesion</h2>
-                        <div className='mb-3'>
-                            <label className='form-label'>Correo Electrónico</label>
-                            <input className='form-control' name='email' type='emailHelp' value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </div>
-                        <div className='mb-3'>
-                            <label className='form-label' >Contraseña</label>
-                            <input className='form-control' name='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </div>
-                        <div className='mb-2'>
-                            <button className='btn btn-success' type='submit' >Iniciar Sesión</button>
-                        </div>
-                        <div className='text-wrd'>
-                            <p >¿No tienes cuenta?</p>
-                            <a className='text-link' href='/registro'>Registrate</a>
-                        </div>
-                    </form>
+                <form onSubmit={handleSubmit}>
+                    <h2>Iniciar Sesion</h2>
+                    <div className='mb-3'>
+                        <label className='form-label'>Correo Electrónico</label>
+                        <input className='form-control' name='email' type='emailHelp' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label' >Contraseña</label>
+                        <input className='form-control' name='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className='mb-2'>
+                        <button className='btn btn-success' type='submit' >Iniciar Sesión</button>
+                    </div>
+                    <div className='text-wrd'>
+                        <p >¿No tienes cuenta?</p>
+                        <a className='text-link' href='/registro'>Registrate</a>
+                    </div>
+                </form>
             </div>
         </div>
     );
