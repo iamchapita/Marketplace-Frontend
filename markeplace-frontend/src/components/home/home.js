7d3741af456836a473552f35a41d7ece6a5f822a
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2023-03-01 15:04:41 -0600

    se avanzo en el modulo de inicio

--- markeplace-frontend/src/components/home/home.js
+++ markeplace-frontend/src/components/home/home.js
@@ -1,34 +1,189 @@
-import React, { useState , useEffect } from "react";
+import React from "react";
+import '../../style/style-home.css';
 import apiClient from '../../services/apiClient';
+import Card from "./card";
+import Search from '../bar/search';
+
 
 function Home(){
+
+    const producto = {
+            name: 'xiaomi',
+            photo: '../../img-test/xiaomi.jpg',
+            price : '7999',
+            description : 'Es un telefono',
+            id : '1'
+        }
+    /**  
     const [products, setProducts] = useState('');
     useEffect( ()=>{
-        getProducts()}, []);
+        getproducts()}, []);
 
 
     const getProducts = async() =>{
-        const res = await apiClient('api/products');
+        const res = await apiClient('api/product');
         setProducts(res.data);
         
-
+        
+        
     }
+    */
     return(
         <div>
+            <nav className="navbar  navbar-expand-lg bg-body-tertiary  ">
+                <div className="container-fluid  " >
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="navbar-brand text-white" href="/productos/categoria1" >Categoria 1</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="navbar-brand text-white" href="/productos/categoria1" >Categoria 2</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="navbar-brand text-white" href="/productos/categoria1"  >Categoria 3</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="navbar-brand text-white" href="/productos/categoria1" >Categoria 4</a>
+                        </li>
+                        
+
+                    </ul>
+                    <Search></Search>
+
+                </div>
+
+            </nav>
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
 
 
-        <div className="card" >
-        
-        <div className="card-body">
-            <h5 className="card-title">{products.name}</h5>
-            <p className="card-text">{products.description}</p>
-            <p className="card-text">{products.price}</p>
-            <a href="/" className="btn btn-primary">detalles</a>
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+
+            <div className="card container-card" >
+                <Card
+                name = {producto.name}
+                photo = {producto.photo}
+                price = {producto.price}
+                description = {producto.description}
+                id = {producto.id}
+                ></Card>
+            </div>
+
+
+            
+
         </div>
-        </div>
 
 
-        </div>
     );
 
 }
