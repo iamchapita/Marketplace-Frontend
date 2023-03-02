36c66054bfe24ad727b352ea194b64e3ef150e8b
iamchapita <alejandrom646@gmail.com>
2023-03-02 06:00:19 -0600

    Implementacion de select de departamento y municipio de forma dinamica

--- markeplace-frontend/src/components/auth/registration.js
+++ markeplace-frontend/src/components/auth/registration.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import apiClient from '../../services/apiClient';
 import UserAgreement from './useragreement';
 import '../../style/style-registration.css'
@@ -11,10 +11,28 @@ function RegistrationPage() {
     const [lastName, setLastName] = useState('');
     const [phoneNumber, setphoneNumber] = useState('');
     const [birthDate, setBirthDate] = useState('');
-    const [addressdepartment, setAddressdepartment] = useState('');
-    const [addresscity, setAddresscity] = useState('');
+    const [addressDepartment, setAddressDepartment] = useState('');
+    const [addressMunicipality, setAddressMunicipality] = useState('');
     const [dni, setDni] = useState('');
     const [isDisabled, setIsDisabled] = useState(false);
+    const [departments, setDepartments] = useState([]);
+    const [municipalities, setMunicipalities] = useState([]);
+
+    useEffect(() => {
+        apiClient.get('api/departments').then((response) => {
+            setDepartments(response.data);
+        })
+    }, []);
+
+    useEffect(() => {
+        apiClient.get('api/municipalities').then((response) => {
+            setMunicipalities(response.data);
+        })
+    }, []);
+
+    const handleChange = (e) => {
+        setAddressDepartment(e.target.value);
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -46,107 +64,117 @@ function RegistrationPage() {
 
                         <div className='col-6'>
                             <label className='form-label'>Nombres</label>
-                            <input 
-                            className='form-control' 
-                            type="text" 
-                            value={name} 
-                            id="name"
-                            name="name"
-                            required
-                            pattern='/([\w \,\+\-\/\#\$\(\)]+)/'
-                            maxlength="45"
-                            onChange={(e) => setName(e.target.value)} />
+                            <input
+                                className='form-control'
+                                type="text"
+                                value={name}
+                                id="name"
+                                name="name"
+                                required
+                                pattern='/([\w \,\+\-\/\#\$\(\)]+)/'
+                                maxlength="45"
+                                onChange={(e) => setName(e.target.value)} />
                         </div>
 
                         <div className='col-6'>
                             <label className='form-label'>Apellidos</label>
-                            <input 
-                            className='form-control' 
-                            type="text" 
-                            value={lastName} 
-                            id="lastName"
-                            name="lastName"
-                            required
-                            pattern='/([\w \,\+\-\/\#\$\(\)]+)/'
-                            maxlength="45"
-                            onChange={(e) => setLastName(e.target.value)} />
+                            <input
+                                className='form-control'
+                                type="text"
+                                value={lastName}
+                                id="lastName"
+                                name="lastName"
+                                required
+                                pattern='/([\w \,\+\-\/\#\$\(\)]+)/'
+                                maxlength="45"
+                                onChange={(e) => setLastName(e.target.value)} />
                         </div>
 
                         <div><p></p></div>
 
                         <div className='col-md-4'>
                             <label className='form-label'>DNI</label>
-                            <input 
-                            className='form-control' 
-                            type="text" 
-                            value={dni}
-                            pattern="[0-9]+{4}\-[0-9]+{4}\- [0-9]+{5}"
-                            required
-                            onChange={(e) => setDni(e.target.value)} />
+                            <input
+                                className='form-control'
+                                type="text"
+                                value={dni}
+                                pattern="[0-9]+{4}\-[0-9]+{4}\- [0-9]+{5}"
+                                required
+                                onChange={(e) => setDni(e.target.value)} />
                         </div>
 
                         <div className='col-md-4'>
                             <label className='form-label'>Teléfono</label>
-                            <input className='form-control' 
-                            type="number" 
-                            value={phoneNumber} 
-                            pattern="[0-9]+{4}\-[0-9]+{4}"
-                            required
-                            onChange={(e) => setphoneNumber(e.target.value)} />
+                            <input className='form-control'
+                                type="number"
+                                value={phoneNumber}
+                                pattern="[0-9]+{4}\-[0-9]+{4}"
+                                required
+                                onChange={(e) => setphoneNumber(e.target.value)} />
                         </div>
 
                         <div className='col-md-4'>
                             <label className='form-label'>Fecha de Nacimiento</label>
-                            <input className='form-control' 
-                            type="date" 
-                            value={birthDate} 
-                            required
-                            onChange={(e) => setBirthDate(e.target.value)} />
+                            <input className='form-control'
+                                type="date"
+                                value={birthDate}
+                                required
+                                onChange={(e) => setBirthDate(e.target.value)} />
                         </div>
 
                         <div><p></p></div>
 
                         <div className='col-6'>
                             <label className='form-label'>Departamento</label>
-                            <select className='form-select' 
-                            type="list" 
-                            value={addressdepartment} 
-                            required
-                            onChange={(e) => setAddressdepartment(e.target.value)} />
+                            <select className='form-select'
+                                type="list"
+                                value={addressDepartment}
+                                required
+                                onChange={handleChange}>
+                                <option value="">Seleccione un Departamento</option>
+                                {departments.map((department) => (
+                                    <option key={department.id} value={department.id}>{department.name}</option>
+                                ))}
+                            </select>
                         </div>
 
                         <div className='col-6'>
                             <label className='form-label'>Municipio</label>
-                            <select 
-                            className='form-select' 
-                            type="list" 
-                            value={addresscity} 
-                            required
-                            onChange={(e) => setAddresscity(e.target.value)} />
+                            <select
+                                className='form-select'
+                                type="list"
+                                value={addressMunicipality}
+                                required
+                                onChange={(e) => setAddressMunicipality(e.target.value)}>
+                                <option value="">Seleccione un Municipio</option>
+                                {municipalities.map((municipality) => (
+                                    <option hidden={municipality.departmentIdFK != addressDepartment} key={municipality.id} value={municipality.id}>{municipality.name}</option>
+                                ))}
+                            </select>
                         </div>
 
                         <div><p></p></div>
 
                         <div className='mb-3'>
                             <label className='form-label'>Correo electronico</label>
-                            <input 
-                            className='form-control' 
-                            type="email" 
-                            value={email} 
-                            required
-                            maxlength="50"
-                            onChange={(e) => setEmail(e.target.value)} />
+                            <input
+                                className='form-control'
+                                type="email"
+                                value={email}
+                                required
+                                maxlength="50"
+                                onChange={(e) => setEmail(e.target.value)} />
                         </div>
 
                         <div className='mb-3'>
                             <label className='form-label'>Contraseña</label>
-                            <input 
-                            className='form-control' 
-                            type="password" 
-                            value={password} 
-                            required
-                            maxlength="50"
-                            onChange={(e) => setPassword(e.target.value)} />
+                            <input
+                                className='form-control'
+                                type="password"
+                                value={password}
+                                required
+                                maxlength="50"
+                                onChange={(e) => setPassword(e.target.value)} />
                         </div>
 
                         <div className='agreements'>
