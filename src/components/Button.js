7c1bf26222670ec08086eca2a43a5b3b12718d6e
iamchapita <alejandrom646@gmail.com>
2023-03-04 19:49:27 -0600

    Correccion de cambio de nombre de variable

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Button = ({ type, fieldName, onClick }) => {
+const Button = ({ type, fieldLabel, onClick }) => {
 
     return(
-        <button className='btn btn-success'type={type} onClick={onClick}>{fieldName}</button>
+        <button className='btn btn-success'type={type} onClick={onClick}>{fieldLabel}</button>
     );
 
 }
