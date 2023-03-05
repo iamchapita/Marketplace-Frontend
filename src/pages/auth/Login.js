51e2c97a6ec57481093c95e3c9ee4f1e0d369675
iamchapita <alejandrom646@gmail.com>
2023-03-05 13:59:51 -0600

    Implementacion de manejo de error de inicio de sesion

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -3,17 +3,20 @@ import { useEffect, useState } from 'react';
 import apiClient from '../../utils/apiClient';
 import InputText from '../../components/InputText';
 import Button from '../../components/Button';
+import Alert from '../../components/common/Alert';
 
 
 const Login = ({ isLoggedIn, setLoggedIn }) => {
 
     const [emailValue, setEmailValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
 
     const handleEmailChange = (e) => {
         setEmailValue(e.target.value);
     }
-    
+
     const handlePasswordChange = (e) => {
         setPasswordValue(e.target.value);
     }
@@ -27,30 +30,31 @@ const Login = ({ isLoggedIn, setLoggedIn }) => {
                     email: emailValue,
                     password: passwordValue
                 }).then((response) => {
-                    console.log(response);
+                    // console.log(response);
                     if (response.status === 200) {
-                        setLoggedIn(true)
-                    } else {
-                        setLoggedIn(false);
+                        setLoggedIn(true);
+                        // Aqui falta redireccionar a pagina principal
                     }
                 }).catch((error) => {
-                    console.log(error);
+                    setShowAlert(true);
+                    setAlertMessage(error.response.data.message);
                 })
             } catch (error) {
-                console.error(error);
+                
             }
         }
-        
+
         action();
 
     }
 
     return (
         <form encType='multipart/form-data'>
+            <Alert text={alertMessage} showAlert={showAlert} setShowAlert={setShowAlert} />
             <InputText type={'email'} fieldLabel={'Correo Electrónico'} fieldName={'email'} placeholder={'micorreo@dominio.com'} inputValue={emailValue} required={true} onChangeHandler={handleEmailChange} />
-            
+
             <InputText type={'password'} fieldLabel={'Contraseña'} fieldName={'password'} placeholder={'Contraseña de entre 8 y 35 caractéres'} inputValue={passwordValue} required={true} onChangeHandler={handlePasswordChange} />
-            <Button type={'submit'} fieldName={'Iniciar Sesión'} onClick={submitHandler}/>
+            <Button type={'submit'} fieldLabel={'Iniciar Sesión'} onClick={submitHandler} />
         </form>
     );
 
