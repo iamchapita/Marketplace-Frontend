53183ea56ebfed2b5b1c120534be73ef1990e054
iamchapita <alejandrom646@gmail.com>
2023-03-04 19:50:00 -0600

    Correcion en estilo de class

--- src/components/Date.js
+++ src/components/Date.js
@@ -6,14 +6,14 @@ const Date = ({ fieldLabel, fieldName, inputValue, onChangeHandler, required = t
         return (
             <div className="input-group mb-3">
                 <label className="input-group-text" htmlFor={fieldName}>{fieldLabel}</label>
-                <input type='date' className='date form-select' name={fieldName} autoComplete='off' value={inputValue} onChange={onChangeHandler} min={min} max={max} required/>
+                <input type='date' className='date form-control' name={fieldName} autoComplete='off' value={inputValue} onChange={onChangeHandler} min={min} max={max} required/>
             </div>
         );
     }else{
         return (
             <div className="input-group mb-3">
                 <label className="input-group-text" htmlFor={fieldName}>{fieldLabel}</label>
-                <input type='date' className='date form-select' name={fieldName} autoComplete='off' value={inputValue} onChange={onChangeHandler} min={min} max={max}/>
+                <input type='date' className='date form-control' name={fieldName} autoComplete='off' value={inputValue} onChange={onChangeHandler} min={min} max={max}/>
             </div>
         );
     }
