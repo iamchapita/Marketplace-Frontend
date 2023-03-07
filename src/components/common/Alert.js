fa0b2e0c6aa4dc7d6ad3206e7abc29e878efb069
iamchapita <alejandrom646@gmail.com>
2023-03-07 16:54:23 -0600

    Eliminacion de retorno de codigo

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -6,14 +6,11 @@ const Alert = ({ text, showAlert, setShowAlert }) => {
     if (showAlert) {
         return (
             <div className="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong>Ups!</strong> {text}
-                <button type="button" className="btn-close" onClick={() => setShowAlert(false)} data-bs-dismiss="alert" aria-label="Close"></button>
+                <strong>Ups!</strong><p dangerouslySetInnerHTML={{__html: text}}></p>
+                <button type="button" className="btn-close" onClick={() => setShowAlert(!showAlert)} data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         );
-    } else {
-        return (<div></div>);
     }
 }
-
 
 export default Alert;
