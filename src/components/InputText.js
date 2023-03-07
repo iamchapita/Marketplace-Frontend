f94de0c31da9d0a7e2cb8fd04145b2b3bfd77871
iamchapita <alejandrom646@gmail.com>
2023-03-07 16:53:43 -0600

    Implementacion de condicion de validez

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const InputText = ({ type, fieldLabel, fieldName, placeholder, inputValue, onChangeHandler, required = true }) => {
+const InputText = ({ type, fieldLabel, fieldName, placeholder, inputValue, onChangeHandler, required = true, isValid = true }) => {
 
     if (required) {
         return (
             <div className="input-group mb-3">
                 <span className="input-group-text" id="basic-addon1">{fieldLabel}</span>
-                <input type={type} className="form-control" placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} value={inputValue} autoComplete="off" required onChange={onChangeHandler}/>
+                <input type={type} className={`form-control ${isValid ? '': "invalid"}`} placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} value={inputValue} autoComplete="off" required onChange={onChangeHandler}/>
             </div>
         );
     } else {
         return (
             <div className="input-group mb-3">
                 <span className="input-group-text" id="basic-addon1">{fieldLabel}</span>
-                <input type={type} className="form-control" placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} value={inputValue} autoComplete="off" onChange={onChangeHandler}/>
+                <input type={type} className={`form-control ${isValid ? '': "invalid"}`} placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} value={inputValue} autoComplete="off" onChange={onChangeHandler}/>
             </div>
         );
     }
