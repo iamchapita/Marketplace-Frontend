f56f3a5cf1f3620958e0b55aa39b935cebbc63b5
Eduardo Salgado <edusalgado00@gmail.com>
2023-03-06 18:57:36 -0600

    Avance en la Navbar, LEER COMENTARIO
    si no le sale nada al darle el boton para el sidebar, en la terminal instalar lo siguiente:
    
    npm install bootstrap jquery popper.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,53 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 
-const Navbar = () =>{
-
-    return(
-        <div>
-            <nav className="navbar">
-                <div className="container-fluid">
-                <a className="navbar-item" href="/">Inicio</a>
-                <a className="navbar-item" href="/register">Registro</a>
-                <a className="navbar-item" href="/login">Inicio de Sesión</a>
-                <button className="toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                </div>
-            </nav>
-        </div>
+function Navbar() {
+    return (
+      <div>
+        <nav className="navbar">
+  <div className="container-fluid">
+    <a className="navbar-item" href="/home">MarketPlace</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">MarketPlace</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/login">Inicio de Sesión</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/register">Registro</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul className="dropdown-menu dropdown-menu-dark">
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li>
+              </li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+      </div>
     );
-}
+  }
+  
 
 export default Navbar;
