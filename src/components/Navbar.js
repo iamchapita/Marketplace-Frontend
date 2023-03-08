63f21aefbae8a58a32e1784f250b187635ed1a28
iamchapita <alejandrom646@gmail.com>
2023-03-08 03:46:14 -0600

    Implementacion de Cierre de Inicio de Sesion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import apiClient from '../utils/apiClient';
 
 
-const Navbar = ({isLoggedIn, setLoggedIn}) => {
+const Navbar = ({ isLoggedIn, setLoggedIn }) => {
+
+    const onLogout = (e) => {
+        e.preventDefault();
+        const registerUser = async () => {
+            const logout = await apiClient.get('/logout').then(response => {
+                console.log(response);
+            })
+        }
+
+        registerUser();
+    }
+
+    const onLogin = () => {};
+
     return (
         <div>
             <nav className="navbar">
@@ -22,10 +37,10 @@ const Navbar = ({isLoggedIn, setLoggedIn}) => {
                         <div className="offcanvas-body">
                             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                                 <li className="nav-item">
-                                    <a className="sidebar-item" aria-current="page" href="/login">Inicio de Sesión</a>
+                                    <a className="sidebar-item" aria-current="page" href={isLoggedIn ? '/logout' : '/login'} onClick={isLoggedIn ? onLogout : onLogin}>{isLoggedIn ? 'Cerrar Sesión' : 'Inicio de Sesión'}</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="sidebar-item" href="/register">Registro</a>
+                                    {isLoggedIn ? <a className="sidebar-item" href="/">Perfil</a> : <a className="sidebar-item" href="/register">Registro</a>}
                                 </li>
                             </ul>
                         </div>
