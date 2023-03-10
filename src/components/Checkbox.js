e225030e4a6da966e2faa6d7ecf6fb66929d411c
iamchapita <alejandrom646@gmail.com>
2023-03-09 22:39:11 -0600

    Correcion: Cambio de propiedad value a propiedad checked

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const Checkbox = ({ fieldLabel, fieldName, inputValue, onChangeHandler, required = true }) => {
+const Checkbox = ({ fieldLabel, fieldName, checkedValue, onChangeHandler, required = true }) => {
 
     if (required) {
         return (
             <div className="mb-3 form-check">
                 <label className="form-check-label" htmlFor={fieldName}>{fieldLabel}</label>
-                <input type="checkbox" className="form-check-input" value={inputValue} onChange={onChangeHandler} required/>
+                <input type="checkbox" className="form-check-input" checked={checkedValue} onChange={onChangeHandler} required/>
             </div>
         );
     } else {
         return (
             <div className="mb-3 form-check">
                 <label className="form-check-label" htmlFor={fieldName}>{fieldLabel}</label>
-                <input type="checkbox" className="form-check-input" value={inputValue} onChange={onChangeHandler} />
+                <input type="checkbox" className="form-check-input" checked={checkedValue} onChange={onChangeHandler} />
             </div>
         );
     }
