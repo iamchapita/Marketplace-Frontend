83e6b92152e08aeb428e375ea4f0043faf001522
iamchapita <alejandrom646@gmail.com>
2023-03-10 18:44:29 -0600

    Vista de Detalle de Producto

---
+++ src/pages/product/ProductDetail.js
@@ -0,0 +1,54 @@
+import React from "react";
+import apiClient from "../../utils/apiClient";
+import { Carousel } from "react-bootstrap";
+
+const ProductDetail = () => {
+
+    return (
+        <div className="container-sm">
+            <div className="row align-items-start">
+                <div className="col-sm-12 col-md-12 col-lg-6">
+                    <div id="carouselExampleIndicators" className="carousel slide">
+                        <div className="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner" style={{ height: '600px'}}>
+                            <div className="carousel-item active">
+                                <img src={process.env.PUBLIC_URL + '/logo512.png'} className="d-block w-100" alt="..." style={{ objectFit: 'cover' }}/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={process.env.PUBLIC_URL + '/unnamed.jpg'} className="d-block w-100" alt="..." style={{ objectFit: 'cover' }}/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={process.env.PUBLIC_URL + '/1.png'} className="d-block w-100" alt="..." style={{ objectFit: 'cover' }}/>
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+
+                </div>
+                <div className="col-sm-10 col-md-10 col-lg-6">
+                    <h2>Nombre del producto</h2>
+                    <p>Vendedor: Nombre del vendedor</p>
+                    <h3>Precio: $XXX</h3>
+                    <p>Ubicación: Ciudad, Estado</p>
+                    <p>Estado del producto: Nuevo/Usado</p>
+                    <button type="button" className="btn btn-primary">Agregar al carrito</button>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
+
+export default ProductDetail;
