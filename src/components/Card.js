2c6f2ea37fb1c56b9ce8da78e6707d2e63e3d8c8
Eduardo Salgado <edusalgado00@gmail.com>
2023-03-14 19:18:25 -0600

    Cambios CSS y Home

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,30 +13,38 @@ function Card ({name, price, description, img, urlDetalles, id, nameSeller, idSe
     
     return(
         
-
-        <div className="container card-publication">
-        <div className="account-container">
-            <CountBar 
-            name={nameSeller}
-            id={idSeller}
-            />
-        </div>    
-        
-        <div className="card">
-            <div className="carousel-item active">
-            <img className="img-fluid" src={img}/>
-            </div>
-            <div className="card-body" key={id} >
-                <h5 className="card-title">{name}</h5>
-                <h6 className="card-text" >{price}</h6>
-                <p className="card-text" >{description}</p>
-                <a className="card-link"  href={urlDetalles}>Detalles</a>
-                <div className="check-container">
-                <label for={`check${id}`}><i className={`material-icons ${favoriteClass ? 'icon-favorite-active ': 'icon-favorite'}`} >favorite</i></label>
-                <input className="check-invisible" type="checkbox" id={`check${id}`}  />
-                </div>
-            </div>
+        <div className="container-sm">
+        <div className="row"> 
+            <div className="col-3">
             
+            </div>
+            <div className="col-sm">   
+                <div className="container card-publication">
+                <div className="account-container">
+                    <CountBar 
+                    name={nameSeller}
+                    id={idSeller}
+                    />
+                </div>    
+                
+                <div className="card">
+                    <div className="carousel-item active">
+                    <img className="img-fluid" src={img}/>
+                    </div>
+                    <div className="card-body" key={id} >
+                        <h5 className="card-title">{name}</h5>
+                        <h6 className="card-text" >{price}</h6>
+                        <p className="card-text" >{description}</p>
+                        <a className="card-link"  href={urlDetalles}>Detalles</a>
+                        <div className="check-container">
+                        <label for={`check${id}`}><i className={`material-icons ${favoriteClass ? 'icon-favorite-active ': 'icon-favorite'}`} >favorite</i></label>
+                        <input className="check-invisible" type="checkbox" id={`check${id}`}  />
+                        </div>
+                    </div>
+
+                </div>    
+            </div>
+            </div>
         </div>
         </div>
 
