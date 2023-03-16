147f2a6b648c16a8205c14865439ec29b124f54c
Eduardo Salgado <edusalgado00@gmail.com>
2023-03-16 00:06:07 -0600

    Implementacion Backend en lista de deseos
    Falta arreglar bien

--- src/components/CardWishList.js
+++ src/components/CardWishList.js
@@ -1,10 +1,56 @@
 import React, {useState, useEffect} from "react";
 import CountBar from "./CountBar";
+import apiClient from "../utils/apiClient";
 
 
 function CardWishList ({name, price, description, img, urlDetalles, id, nameSeller, idSeller, favoriteClass}){
   
+    const[favoriteClassWL, setFavoriteClassWL] = useState(false);
+    const[userId, setUserId] =useState();
+    var [nameUser, setNameUser] =useState('');
+
+    const user = () =>{
+        apiClient.get('/user').then((res)=>{
+            //console.log(res.data.id);
+            setUserId(res.data.id);
+        });
+       
+    }
+
+    const getNameUser = async (idSeller) => {
+        await apiClient.post('/sellerDetails', {
+            id : idSeller
+        }).then((res)=>{
+            setNameUser(res.data.name);
+        });
+    }
+
+    const favorite = async (id) =>{
+
+        var checkValue = document.getElementById('check'+id).checked;
+        var addedDate = new Date();
+        setFavoriteClassWL(checkValue);
+        if(favoriteClass){
+        await apiClient.post('/wishlistInsert', {
+            productIdFK : id,
+            userIdFK : userId
+        });
+        alert('se agregó a lista de favoritos');
+        }else{
+        await apiClient.post('/wishlistDelete',  {
+            productIdFK : id,
+            userIdFK : userId,
+        });
+        alert('se eliminó de la lista de deseos')
+    }
+    }
     
+ 
+    useEffect(()=>{
+      getNameUser(idSeller);
+      user();
+    },[]);
+
     return(
         
         <div className="container-WL">
@@ -12,7 +58,7 @@ function CardWishList ({name, price, description, img, urlDetalles, id, nameSell
                 <div className="row">
                 <div className="col-3">
                         <div>
-                                <img className="img-wl" src={img}/>
+                            <img className="img-wl" src={img}/>
                         </div>
                 </div>
                 <div class="col-8">
@@ -22,8 +68,8 @@ function CardWishList ({name, price, description, img, urlDetalles, id, nameSell
                             <p className="card-text" >{description}</p>
                             <a className="card-link"  href={urlDetalles}>Detalles</a>
                             <div className="check-container">
-                            <label for={`check${id}`}><i className={`material-icons ${favoriteClass ? 'icon-favorite-active ': 'icon-favorite'}`} >favorite</i></label>
-                            <input className="check-invisible" type="checkbox" id={`check${id}`}  />
+                            <label id={`labelCheck${id}`}  htmlFor={`check${id}`}><i className={`material-icons ${favoriteClass ? 'icon-favorite-active ': 'icon-favorite'}`} >favorite</i></label>
+                            <input className="check-invisible" onClick={()=>favorite(id)} type="checkbox" id={`check${id}`}  />
                             </div>
                         </div>
                 </div>    
--- src/pages/wishlist/WishList.js
+++ src/pages/wishlist/WishList.js
@@ -1,97 +1,53 @@
 import React, {useState, useEffect} from "react";
 import CardWishList from "../../components/CardWishList";
 import images from "../../utilities/json-images/images";
-
+import apiClient from "../../utils/apiClient";
 
 
 function WishList (){
 
-    var userRegister = {'name' : 'Demsey Euceda ', 'id' : '1'};
-    
-    var data = [
-        {'id': '1',
-        'name' : 'Huawei P30', 
-        'price' : '5999', 
-        'description' : 'Smartphone de alta calidad camara de 64mpx Completamente nuevo',
-        'img' : images.huawei,
-        'user' : {name : 'Demsey Euceda', 
-                  id : "1"  }
-    },
-    
-        {'id': '2',
-        'name' : 'Iphone X', 
-        'price' :'10999', 
-        'description' : 'Smartphone de alta calidad camara de 64mpx Completamente nuevo',
-        'img' : images.samsung,
-        'user' : {name : 'Demsey Euceda', 
-                  id : "1"  }
-    },
-    
-        {'id': '2',
-        'name' : 'Machete de acero', 
-        'price' : '500', 
-        'description' : 'machete nuevo para cortar cesped o madera',
-        'img' : images.machete,
-        'user' : {name : 'Demsey Euceda', 
-                  id : "1"  }
-    },
-    
-        {'id': '3',
-        'name' : 'Mochila', 
-        'price' : '1000', 
-        'description' : 'muchilas nuevas para escuela',
-        'img' : images.mochila,
-        'user' : {name : 'Eduardo', 
-                  id : "1"  }
-    },
-    
-        {'id': '4',
-        'name' : 'Samsung s10', 
-        'price' : '5999', 
-        'description' : 'Smartphone de alta calidad camara de 64mpx Completamente nuevo',
-        'img' : images.samsung, 
-        'user' : {name : 'Alejandra Galo', 
-                  id : "1"  }
-        
-                
+    var [products , setProduts]=useState(['']);
+    //var [user, setUser]=useState('');
+
+    const getProducts =async() =>{
+        apiClient.get('/products').then((res)=>{
+            setProduts(res.data[0]);
+            console.log(res.data[0]);
+        })
     }
-    ];
-    var [products , setProduts]=useState([]);
-    var [user, setUser]=useState('')
-   // var checkValue = document.getElementById('check'+products[1].id).checked;
-    //console.log(checkValue);
-    //console.log(images.huawei)
+    const getUserName = async (id) =>{
+        
+    }
     useEffect(()=>{
-        setProduts(data);
-        setUser(userRegister);
-        
-        
-        //console.log(data);
-        //console.log(products)
+        getProducts();
+
         
     },[]);
     return(
 
         <div>
-            <div>
-                <div className="container wish">
-                    {
-                    products.map ((product)=>(
+            
+            <div className="container home">
+               
+            {
+                products.map ((product, id)=>(
+                    
                     <CardWishList
-                    nameSeller={product.user.name}
-                    idSeller={product.user.id}
                     key={product.id}
+                    id ={product.id}
+                    nameSeller={getUserName}
+                    idSeller={product.userIdFK}
                     name = {product.name}
                     price =  {product.price}
                     description = {product.description}
-                    img = {product.img}
-                    urlDetalles = {`/product/${product.id}`}
-                    favoriteClass = {true}
+                    img = {product.photo}
+                    urlDetalles = {`/productDetail/${product.id}`}
                     />
-                    ))
-                    }
-                </div>
+                ))
+            }
+            
             </div>
+
         </div>
     );
 }
