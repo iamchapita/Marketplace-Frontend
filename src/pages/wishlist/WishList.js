ad721e1b5a655e6227a788ad285f8374f90f9aea
iamchapita <alejandrom646@gmail.com>
2023-03-20 21:14:48 -0600

    Implementacion de wishList

--- src/pages/wishlist/WishList.js
+++ src/pages/wishlist/WishList.js
@@ -1,64 +1,61 @@
-import React, {useState, useEffect, lazy, Suspense} from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import { Spinner } from "react-bootstrap";
-
-//import CardWishList from "../../components/CardWishList";
-//import images from "../../utilities/json-images/images";
 import apiClient from "../../utils/apiClient";
 
-const CardWishList = lazy(()=>import("../../components/CardWishList"));
+const CardWishList = lazy(() => import("../../components/CardWishList"));
 
 
-function WishList (){
+function WishList() {
 
-    var [products , setProduts]=useState(['']);
-    var [user, setUser]=useState('');
- 
-    const getWishlist = async (id) =>{
-        apiClient.post('/wishlist', {userIdFK : id}).then((res)=>{
-            setProduts(res.data);
-            
+    const [products, setProduts] = useState([]);
+    const [user, setUser] = useState(' ');
 
-               
-        })
-    }
-
-    const getUser = () =>{
-        apiClient.get('/user').then((res)=>{  
-            setUser(res.data);
-        });
-       
-    }
-
-
-    useEffect(()=>{
+    useEffect(() => {
+        const getUser = async () => {
+            const response = await apiClient.get('/user').then((res) => {
+                setUser(res.data);
+            }).catch((error) => {
+                if (error.response.status === 401) {
+                    console.log('Debe Iniciar Sesión.');
+                }
+            })
+        }
         getUser();
-        getWishlist(user.id);
-        
+    }, []);
 
-        
-    },[]);
-    return(
+    useEffect(() => {
+        const getWishlist = async () => {
+            const response = await apiClient.post('/wishlist', { userIdFK: user.id }).then((res) => {
+                setProduts(res.data);
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+        getWishlist();
+
+    }, [user]);
+
+    return (
 
         <div>
-            
+
             <div className="container home">
-               
-            {
-                products.map ((product, id)=>(
-                    <Suspense fallback={<Spinner/>} >
-                    <CardWishList
-                    key={id}
-                    id ={product.id}
-                    idSeller={product.userIdFK}
-                    name = {product.name}
-                    price =  {product.price}
-                    img = {product.photos}
-                    urlDetalles = {`/productDetail/${product.id}`}
-                    />
-                    </Suspense>
-                ))
-            }
-            
+
+                {
+                    products.map((product, id) => (
+                        <Suspense key={id} fallback={<Spinner />} >
+                            <CardWishList
+                                id={product.id}
+                                idSeller={product.userIdFK}
+                                name={product.name}
+                                price={product.price}
+                                img={product.photos}
+                                urlDetalles={`/productDetail/${product.id}`}
+                            />
+                        </Suspense>
+                    ))
+                }
+
             </div>
 
         </div>
