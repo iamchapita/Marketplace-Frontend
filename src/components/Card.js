adc10afca29017210b8dc6d054d54482bf997537
iamchapita <alejandrom646@gmail.com>
2023-03-26 16:33:52 -0600

    Correcion de funcion de wishlist en vista home

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import apiClient from "../utils/apiClient";
+import { Spinner } from "react-bootstrap";
 
-function Card({ id, userId, name, price, urlDetalles, path, idSeller, nameSeller, heart = null }) {
+function Card({ id, userId, name, price, urlDetalles, path, idSeller, nameSeller, heart, isWhisListStatusInclude = false }) {
 
     const [productImage, setProductImage] = useState([]);
     const [productExtension, setProductExtension] = useState('');
     const [favoriteClass, setFavoriteClass] = useState(heart);
+    const [isReadyToRender, setIsReadyToRender] = useState(false);
 
     useEffect(() => {
 
@@ -31,6 +33,7 @@ function Card({ id, userId, name, price, urlDetalles, path, idSeller, nameSeller
                     return extension;
                 })
                 setProductExtension(extensions);
+                setIsReadyToRender(true);
             } catch (error) {
                 // setResponseMessage('Error');
                 console.log(error.response.data);
@@ -39,8 +42,8 @@ function Card({ id, userId, name, price, urlDetalles, path, idSeller, nameSeller
     }, [productImage]);
 
     const favorite = async (id) => {
-        let checkValue = !favoriteClass;
-        setFavoriteClass(checkValue);
+        setFavoriteClass(!favoriteClass);
+
         if (!favoriteClass) {
             await apiClient.post('/wishlistInsert', {
                 productIdFK: id,
@@ -58,7 +61,6 @@ function Card({ id, userId, name, price, urlDetalles, path, idSeller, nameSeller
             await apiClient.post('/wishlistDelete', {
                 productIdFK: id,
                 userIdFK: userId
-
             }).then((res) => {
                 if (res.statusText == 'OK') {
                     console.log('Aqui debe haber un alert de bootstrap que se desvanezca');
@@ -70,7 +72,7 @@ function Card({ id, userId, name, price, urlDetalles, path, idSeller, nameSeller
         }
     }
 
-    if (heart !== null) {
+    if (isWhisListStatusInclude) {
         return (
             <div className="container-home">
                 <div className="container card-publication">
@@ -83,9 +85,13 @@ function Card({ id, userId, name, price, urlDetalles, path, idSeller, nameSeller
                         </div>
                         <div className="col-5">
                             {
-                                productImage.map((image, index) => (
+                                !isReadyToRender ? (<div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Spinner animation="border" variant="light" />
+                                    </div>
+                                </div>) : (productImage.map((image, index) => (
                                     <img key={index} src={`data:image/${productExtension[index]};base64,${image.base64Image}`} className="img-fluid" />
-                                ))
+                                )))
                             }
                         </div>
                         <div className="card-body" key={id} >
@@ -94,7 +100,7 @@ function Card({ id, userId, name, price, urlDetalles, path, idSeller, nameSeller
                             <a className="card-link" href={urlDetalles}>Detalles</a>
                             <div className="check-container">
                                 <label id={`labelCheck${id}`} htmlFor={`check${id}`}>
-                                    <i className={`material-icons ${favoriteClass == true ? 'icon-favorite-active ' : 'icon-favorite'}`} >favorite
+                                    <i className={`material-icons ${favoriteClass ? 'icon-favorite-active ' : 'icon-favorite'}`} >favorite
                                     </i>
                                 </label>
                                 <input className="check-invisible" onClick={() => favorite(id)} type="checkbox" id={`check${id}`} />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,27 +1,38 @@
 import React, { useState, useEffect, } from "react";
 import apiClient from "../../utils/apiClient";
 import Card from "../../components/Card";
-import LazyLoad from 'react-lazy-load';
-import { Spinner } from 'react-bootstrap';
+import { Spinner } from "react-bootstrap";
 
 const Home = ({ isLoggedIn }) => {
 
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState(null);
+    const [isReadyToRender, setIsReadyToRender] = useState(false);
+    const [userId, setUserId] = useState(null);
     const [isWhisListStatusInclude, setIsWhisListStatusInclude] = useState(null);
 
-    const userId = localStorage.getItem('id');
+    useEffect(() => {
+        const getUser = async () => {
+            await apiClient.get('/user/').then((response) => {
+                setUserId(response.data.id);
+            }).catch((error) => {
+                setUserId(false);
+                setIsWhisListStatusInclude(false);
+            });
+        }
+        getUser();
+    }, []);
+
+    useEffect(() => {
+
+        if(isLoggedIn === false) {
+            setIsWhisListStatusInclude(false);
+            // setUserId(false);
+        }
+
+    }, [isLoggedIn])
 
     useEffect(() => {
         const getProducts = async () => {
-
-            if (userId) {
-                await apiClient.get(`/productsWishList/${userId}`).then((response) => {
-                    setProducts(response.data);
-                    setIsWhisListStatusInclude(true);
-                }).catch((error) => {
-                    console.log(error);
-                });
-            }
             if (!userId) {
                 await apiClient.get('/products').then((response) => {
                     setProducts(response.data);
@@ -29,76 +40,82 @@ const Home = ({ isLoggedIn }) => {
                 }).catch((error) => {
                     console.log(error);
                 });
+
+            } else {
+                await apiClient.get(`/productsWishList/${userId}`).then((response) => {
+                    setProducts(response.data);
+                    setIsWhisListStatusInclude(true);
+                }).catch((error) => {
+                    console.log(error);
+                });
             }
         }
 
-        getProducts();
+        if (userId !== null) {
+            getProducts();
+        }
 
-    }, [userId]);
+    }, [userId, isLoggedIn]);
 
-    const SpinnerComponent = () => (
-        <div className="container-sm d-flex justify-content-center">
-            <Spinner animation="border" />
-        </div>
-    );
 
-    if (isWhisListStatusInclude) {
-        return(
-            <div>
-                <LazyLoad
-                    height={200}
-                    offset={100}
-                    placeholder={<SpinnerComponent />}
-                >
-                    <div className="container home">
-                        {
-                            products.map((product, id) => (
-                                <Card
-                                    key={id}
-                                    id={product.id}
-                                    userId={userId}
-                                    name={product.name}
-                                    price={product.price}
-                                    urlDetalles={`/productDetail/${product.id}`}
-                                    path={product.photos}
-                                    idSeller={product.userIdFK}
-                                    nameSeller={product.userFirstName + ' ' + product.userLastName}
-                                    heart={Boolean(product.isProductInWishList)}
-                                    // description={product.description}
-                                />
-                            ))
-                        }
-                    </div>
-                </LazyLoad>
+    if (!isReadyToRender) {
+        
+        if(products !== null){
+            setIsReadyToRender(true);
+        }
+
+        return (
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+                <div className="d-flex align-items-center justify-content-center">
+                    <Spinner animation="border" variant="light" />
+                </div>
             </div>
         );
     }
-    if (!isWhisListStatusInclude) {
-        return(
-            <div>
-                <LazyLoad
-                    height={200}
-                    offset={100}
-                    placeholder={<SpinnerComponent />}
-                >
-                    <div className="container home">
-                        {
-                            products.map((product, id) => (
-                                <Card
-                                    key={id}
-                                    id={product.id}
-                                    name={product.name}
-                                    price={product.price}
-                                    urlDetalles={`/productDetail/${product.id}`}
-                                    path={product.photos}
-                                    idSeller={product.userIdFK}
-                                    nameSeller={product.userFirstName + ' ' + product.userLastName}
-                                    // description={product.description}
-                                />
-                            ))
-                        }
-                    </div>
-                </LazyLoad>
+
+    if (isWhisListStatusInclude) {
+        return (
+            <div className="container home">
+                {
+                    products.map((product, id) => (
+                        <Card
+                            key={id}
+                            id={product.id}
+                            userId={userId}
+                            name={product.name}
+                            price={product.price}
+                            urlDetalles={`/productDetail/${product.id}`}
+                            path={product.photos}
+                            idSeller={product.userIdFK}
+                            nameSeller={product.userFirstName + ' ' + product.userLastName}
+                            isWhisListStatusInclude={true}
+                            heart={product.isProductInWishList}
+                        // description={product.description}
+                        />
+                    ))
+                }
+            </div>
+        );
+    }
+
+    if (isWhisListStatusInclude === false) {
+        return (
+            <div className="container home">
+                {
+                    products.map((product, id) => (
+                        <Card
+                            key={id}
+                            id={product.id}
+                            name={product.name}
+                            price={product.price}
+                            urlDetalles={`/productDetail/${product.id}`}
+                            path={product.photos}
+                            idSeller={product.userIdFK}
+                            nameSeller={product.userFirstName + ' ' + product.userLastName}
+                        // description={product.description}
+                        />
+                    ))
+                }
             </div>
         );
     }
