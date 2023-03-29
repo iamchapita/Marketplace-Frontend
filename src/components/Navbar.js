ff076f43dba4649650aa8568cdd6bfc7d823f9ba
iamchapita <alejandrom646@gmail.com>
2023-03-28 18:04:52 -0600

    Implementacion de variables de tipo de usuario

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,10 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import apiClient from '../utils/apiClient';
 
 
-const Navbar = ({ isLoggedIn, setLoggedIn }) => {
+const Navbar = ({ isLoggedIn, setLoggedIn, isAdmin, setIsAdmin, isClient, setIsClient, isSeller, setIsSeller, isBanned, setIsBanned, isEnabled, setIsEnabled }) => {
 
     useEffect(() => {
         const getUser = async () => {
-            // const response = await apiClient.get('/user').then((res) => {
-            //     setLoggedIn(true);
-            // }).catch((error) => {
-            //     setLoggedIn(false);
-            // })
             localStorage.getItem('access_token') ? setLoggedIn(true) : setLoggedIn(false);
         }
         getUser();
@@ -25,53 +20,116 @@ const Navbar = ({ isLoggedIn, setLoggedIn }) => {
         const registerUser = async () => {
             const logout = await apiClient.get('/logout').then(response => {
                 setLoggedIn(false);
+                setIsAdmin(false);
+                setIsClient(false);
+                setIsSeller(false);
+                setIsBanned(false);
+                setIsEnabled(false);
                 localStorage.removeItem('access_token');
                 localStorage.removeItem('id');
             }).catch(error => {
                 console.log(error);
             })
         }
-
         registerUser();
     }
 
-    const onLogin = () => {};
+    const onLogin = () => { };
 
-    return (
-        <div>
-            <nav className="navbar">
-                <div className="container-fluid">
-                    <a className="navbar-item" href="/home">MarketPlace</a>
-                    <button className="toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-                        <span className="toggler-item">M♥</span>
-                    </button>
-                    <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                        <div className="offcanvas-header">
-                            <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">MarketPlace</h5>
-                            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div className="offcanvas-body">
-                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li className="nav-item">
-                                    <a className="sidebar-item" aria-current="page" href={isLoggedIn ? '/logout' : '/login'} onClick={isLoggedIn ? onLogout : onLogin}>{isLoggedIn ? 'Cerrar Sesión' : 'Inicio de Sesión'}</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="sidebar-item" aria-current="page" href={isLoggedIn ? '/logout' : '/register'} onClick={isLoggedIn ? onLogout : onLogin}>{isLoggedIn ? '' : 'Registro de Usuario'}</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="sidebar-item" aria-current="page" href={isLoggedIn ? '/productInsert' : '/productInsert'} onClick={isLoggedIn ? onLogin : onLogout}>{isLoggedIn ? 'Publicar Productos' : ''}</a>
-
-                                </li>
-                                <li className="nav-item">
-                                    <a className="sidebar-item" aria-current="page" href={isLoggedIn ? '/wishlist' : '/wishlist'} onClick={isLoggedIn ? onLogin : onLogout}>{isLoggedIn ? 'Lista de Deseos' : ''}</a>
-                                </li>
-                            </ul>
+    if (!isAdmin) {
+        return (
+            <div>
+                <nav className="navbar">
+                    <div className="container-fluid">
+                        <a className="navbar-item" href="/home">MarketPlace</a>
+                        <button className="toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                            <span className="toggler-item">M♥</span>
+                        </button>
+                        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                            <div className="offcanvas-header">
+                                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">MarketPlace</h5>
+                                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div className="offcanvas-body">
+                                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                    <li className="nav-item">
+                                        <a className="sidebar-item" aria-current="page" href={isLoggedIn ? '/logout' : '/login'} onClick={isLoggedIn ? onLogout : onLogin}>{isLoggedIn ? 'Cerrar Sesión' : 'Inicio de Sesión'}</a>
+                                    </li>
+                                    {
+                                        !isLoggedIn ?
+                                            (
+                                                <li className="nav-item">
+                                                    <a className="sidebar-item" aria-current="page" href='/register' onClick={isLoggedIn ? onLogout : onLogin}>Registro de Usuario</a>
+                                                </li>
+                                            ) : (<div></div>)
+                                    }
+                                    {
+                                        isLoggedIn ?
+                                            (
+                                                isSeller ?
+                                                    (
+                                                        <li className="nav-item">
+                                                            <a className="sidebar-item" aria-current="page" href='/productInsert'>Publicar Producto</a>
+                                                        </li>
+                                                    ) : (
+                                                        <li className="nav-item">
+                                                            <a className="sidebar-item" aria-current="page" href='/productInsert'>Convertirme en Vendedor</a>
+                                                        </li>
+                                                    )
+                                            ) : (<div></div>)
+                                    }
+                                    {
+                                        isLoggedIn ?
+                                            (
+                                                <li className="nav-item">
+                                                    <a className="sidebar-item" aria-current="page" href={isLoggedIn ? '/wishlist' : '/wishlist'} onClick={isLoggedIn ? onLogin : onLogout}>{isLoggedIn ? 'Lista de Deseos' : ''}</a>
+                                                </li>
+                                            ) : (<div></div>)
+                                    }
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </nav>
-        </div>
-    );
+                </nav>
+            </div>
+        );
+    }
+
+    if (isAdmin) {
+        return (
+            <div>
+                <nav className="navbar">
+                    <div className="container-fluid">
+                        <a className="navbar-item" href="/home">MarketPlace</a>
+                        <button className="toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                            <span className="toggler-item">M♥</span>
+                        </button>
+                        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                            <div className="offcanvas-header">
+                                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">MarketPlace</h5>
+                                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div className="offcanvas-body">
+                                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                    {
+                                        isLoggedIn ?
+                                            (
+                                                <li className="nav-item">
+                                                    <a className="sidebar-item" aria-current="page" href={isLoggedIn ? '/logout' : '/login'} onClick={isLoggedIn ? onLogout : onLogin}>{isLoggedIn ? 'Cerrar Sesión' : 'Inicio de Sesión'}</a>
+                                                </li>
+                                            ) : (<div></div>)
+                                    }
+                                    {
+                                        isAdmin ? (<div></div>) : (<div></div>)
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
 }
 
 
