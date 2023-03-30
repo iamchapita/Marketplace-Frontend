4efd2b5c6fa449a083575222b9c7f0425eed1543
Ana Cantarero <isacantarero29@gmail.com>
2023-03-29 20:44:07 -0600

    Implementacion parcial de Card de Producto para la vista de vendedor

---
+++ src/components/SellerProductCard.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import apiClient from "../utils/apiClient";
+import { Spinner } from "react-bootstrap";
+
+function SellerProductCard({ id, name, price, path }) {
+
+    const [productImage, setProductImage] = useState([]);
+    const [productExtension, setProductExtension] = useState('');
+    const [isReadyToRender, setIsReadyToRender] = useState(false);
+
+    useEffect(() => {
+
+        const getProductImage = async () => {
+            await apiClient.post('/getProductImages', {
+                path: path,
+                imagesToObtain: 1
+            }).then((respose) => {
+                setProductImage(respose.data);
+            }).catch((error) => {
+                console.log(error.response.data);
+            });
+        }
+        getProductImage();
+
+    }, [id]);
+
+    useEffect(() => {
+        if (productImage.length !== 0) {
+            try {
+                const extensions = productImage.map((productImageObject) => {
+                    const extension = productImageObject.name.split('.').pop();
+                    return extension;
+                })
+                setProductExtension(extensions);
+                setIsReadyToRender(true);
+            } catch (error) {
+                console.log(error.response.data);
+            }
+        }
+    }, [productImage]);
+
+
+    return (
+        <div key={id} className="col">
+            <div key={id} className="card h-100">
+                {
+                    !isReadyToRender ? (<div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <div className="d-flex align-items-center justify-content-center">
+                            <Spinner animation="border" variant="dark" />
+                        </div>
+                    </div>) : (productImage.map((image, index) => (
+                        <div className="img-card" >
+                            <img key={index} src={`data:image/${productExtension[index]};base64,${image.base64Image}`} className="img-fluid" />
+                        </div>
+                    )))
+                }
+                <div key={id} className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <h6 className="card-text" >{price}</h6>
+                    <a className="card-link" href={`/productDetail/${id}`}>Detalles</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SellerProductCard;
