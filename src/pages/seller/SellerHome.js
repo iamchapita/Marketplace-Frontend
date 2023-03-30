ebd57d2a6ba37cbd25374c53d7386d55607c16f6
Ana Cantarero <isacantarero29@gmail.com>
2023-03-29 19:10:16 -0600

    Renderizado de informacion de usuario

--- src/pages/seller/SellerHome.js
+++ src/pages/seller/SellerHome.js
@@ -7,109 +7,133 @@ const SellerHome = ({ isLoggedIn, setLoggedIn, isClient, setIsClient, isSeller, 
 
     // Variables de estado, Se almacena la informacion a renderizar en la vista
     const [sellerInfo, setSellerInfo] = useState(null);
-    const [products, setProducts] = useState(null);
+    const [products, setProducts] = useState([]);
     const [readyToRender, setReadyToRender] = useState(false);
     const [productsWereFound, setProductsWereFound] = useState(null);
 
+    // Se ejecuta cuando se crea el componente y cuando el valor de isLoggedIn cambie
     useEffect(() => {
         const action = async () => {
+            // Obtiene los productos del vendedor
             const response = await apiClient.post('/getProductsBySeller', {
+                // Envia el id del usuario vendedor. Se obtiene desde localstorage.
+                // Este id se guarda en el componente de login.
                 sellerId: localStorage.getItem('id')
             }).then((response) => {
+                // Se establece el arreglo de productos
                 setProducts(response.data);
                 setProductsWereFound(true);
             }).catch((error) => {
-                if(error.response.status === 500){
-                    setProductsWereFound(true);
-                }else{
+                // Se debe renderizar un error de que no se encontro productos. PENDIENTE.
+                if (error.response.status === 500) {
+                    setProductsWereFound(false);
+                } else {
                     console.log(error.response.data);
                 }
             });
         }
         action();
-    }, [isLoggedIn]);
+    }, []);
 
     useEffect(() => {
-    
-        const sellerInfoObject = {
-            'firstName': products[0]['userFirstName'],
-            'lastName': products[0]['userLastName'],
-            'departmentName': products[0]['departmentName'],
-            'municipalityName': products[0]['municipalityName']
+        // Establece la informacion del usuario del tipo vendedor
+        if (products.length > 0) {
+            const sellerInfoObject = {
+                'firstName': products[0]['userFirstName'],
+                'lastName': products[0]['userLastName'],
+                'departmentName': products[0]['departmentName'],
+                'municipalityName': products[0]['municipalityName']
+            }
+            setSellerInfo(sellerInfoObject);
         }
 
-        setSellerInfo(sellerInfoObject);
-        setReadyToRender(true);
+    }, [products]);
 
-    }, [productsWereFound !== null && productsWereFound !== false])
+    useEffect(() => {
 
+        if (sellerInfo !== null) {
+            setReadyToRender(true);
+        }
 
+    }, [sellerInfo])
 
-    return (
-        <div className='container-fluid' style={{ marginTop: '3em' }}>
-            {
-                !readyToRender ? (
-                    <div className="container d-flex justify-content-center">
-                        <Spinner animation="border" />
-                    </div>
-                ) : (
-                    <div className='row mx-4 my-3'>
-                        <div className='col-md-3'>
-                            <div className='col-md-12 container-style' style={{ margin: '0 0 1em 0' }}>
-                                <ul>
-                                    <li>
-                                        <a href='/'>Home</a>
-                                    </li>
-                                </ul>
-                            </div>
+    if (!readyToRender) {
+        return (
+            <div className='container-fluid' style={{ marginTop: '3em' }}>
+                <div className="container d-flex justify-content-center">
+                    <Spinner animation="border" />
+                </div>
+            </div>
+        )
+    } else {
+
+        return (
+            <div className='container-fluid' style={{ marginTop: '3em' }}>
+                {
+                    !readyToRender ? (
+                        <div className="container d-flex justify-content-center">
+                            <Spinner animation="border" />
                         </div>
-                        <div className='col-md-9'>
-                            <div className='col-md-12 container-style'>
-                                <div className="row row-cols-1 row-cols-md-3 g-4">
-                                    <div className="col">
-                                        <div className="card h-100">
-                                            <img src="..." className="card-img-top" alt="..."></img>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Card title</h5>
-                                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    ) : (
+                        <div className='row mx-4 my-3'>
+                            <div className='col-md-3'>
+                                <div className='col-md-12 container-style' style={{ margin: '0 0 1em 0' }}>
+                                    <div style={{ padding: '1em 1em 1em 1em', wordWrap: 'break-word' }}>
+                                        <div className='row' style={{ color: 'white' }} >Nombre: { sellerInfo.firstName + ' ' + sellerInfo.lastName }</div>
+                                        <div className='row' style={{ color: 'white' }} >Ubicación</div>
+                                        <div className='row' style={{ color: 'white' }} >{ sellerInfo.departmentName }</div>
+                                        <div className='row' style={{ color: 'white' }} >{ sellerInfo.municipalityName }</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-md-9'>
+                                <div className='col-md-12 container-style'>
+                                    <div className="row row-cols-1 row-cols-md-3 g-4">
+                                        <div className="col">
+                                            <div className="card h-100">
+                                                <img src="..." className="card-img-top" alt="..."></img>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">HomeProductCard title</h5>
+                                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="col">
-                                        <div className="card h-100">
-                                            <img src="..." className="card-img-top" alt="..."></img>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Card title</h5>
-                                                <p className="card-text">This is a short card.</p>
+                                        <div className="col">
+                                            <div className="card h-100">
+                                                <img src="..." className="card-img-top" alt="..."></img>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">HomeProductCard title</h5>
+                                                    <p className="card-text">This is a short card.</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="col">
-                                        <div className="card h-100">
-                                            <img src="..." className="card-img-top" alt="..."></img>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Card title</h5>
-                                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                        <div className="col">
+                                            <div className="card h-100">
+                                                <img src="..." className="card-img-top" alt="..."></img>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">HomeProductCard title</h5>
+                                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="col">
-                                        <div className="card h-100">
-                                            <img src="..." className="card-img-top" alt="..."></img>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Card title</h5>
-                                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <div className="col">
+                                            <div className="card h-100">
+                                                <img src="..." className="card-img-top" alt="..."></img>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">HomeProductCard title</h5>
+                                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                )
-            }
-        </div>
-    );
+                    )
+                }
+            </div>
+        );
+    }
 };
 
 export default SellerHome;
