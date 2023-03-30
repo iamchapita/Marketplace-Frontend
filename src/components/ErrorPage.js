8754dd833f77bea7b74fbdd27ca65b5dda915420
iamchapita <alejandrom646@gmail.com>
2023-03-30 03:04:25 -0600

    Refactorizacion de componente

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Row, Col, Alert } from 'react-bootstrap';
 
-const ErrorPage = () => {
+const ErrorPage = ({ title, text }) => {
     return (
         <Container>
             <Row className="justify-content-md-center mt-5">
                 <Col md="8">
                     <Alert variant="danger" className="text-center">
-                        <h1>Acceso denegado</h1>
-                        <p>No tienes permisos para acceder a esta página.</p>
+                        <h1>{title}</h1>
+                        <p>{text}</p>
                     </Alert>
                 </Col>
             </Row>
