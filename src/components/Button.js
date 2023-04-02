1ffdfa6f6e9cbd6e9bb23b7e1155bb0bf0101741
iamchapita <alejandrom646@gmail.com>
2023-04-01 18:12:42 -0600

    Implementación de Tooltip

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
-const Button = ({ type, fieldLabel, onClick, buttonClass = 'success' }) => {
+const Button = ({ type, fieldLabel, onClick, buttonClass = 'success', tooltipText = '' }) => {
 
-    return (
-        <button className={`btn btn-${buttonClass}`} type={type} onClick={onClick}>{fieldLabel}</button>
-    );
+    if (tooltipText.length > 0) {
+        const renderTooltip = (props) => (
+            <Tooltip id="button-tooltip" {...props}>
+                {tooltipText}
+            </Tooltip>
+        );
 
+        return (
+            <OverlayTrigger
+                placement="top"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltip}
+            >
+                <button className={`btn btn-${buttonClass}`} type={type} onClick={onClick}>{fieldLabel}</button>
+            </OverlayTrigger>
+        );
+    } else {
+        return (
+            <button className={`btn btn-${buttonClass}`} type={type} onClick={onClick}>{fieldLabel}</button>
+        );
+    }
 }
 
 export default Button;
