a2fa7627d561cf147533529328f06c27e8594185
iamchapita <alejandrom646@gmail.com>
2023-04-12 00:12:37 -0600

    Correcion en clase css

--- src/pages/admin/modules/UsersModule.js
+++ src/pages/admin/modules/UsersModule.js
@@ -75,7 +75,7 @@ const UsersModule = ({ isLoggedIn, isAdmin, areUserStatusLoaded }) => {
         }
 
         return (
-            <div className='container container-style' style={{ color: 'black' }}>
+            <div className='container-md container-style' style={{ color: 'black' }}>
                 <h1 className="text-center" style={{ color: 'white' }} >Módulo de Usuarios</h1>
                 <div id="registerPerPage">
                     <div className="form-floating">
