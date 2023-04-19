d1d4cfa500cd0394542eb8e6381e830394081f73
iamchapita <alejandrom646@gmail.com>
2023-04-19 00:01:54 -0600

    Implementacion de parametro en la consulta para evitar cache

--- src/pages/admin/modules/UsersModule/UsersModule.js
+++ src/pages/admin/modules/UsersModule/UsersModule.js
@@ -8,17 +8,17 @@ const UsersModule = ({ isLoggedIn, isAdmin, areUserStatusLoaded }) => {
 
     const [registerPerPageValue, setRegisterPerPageValue] = useState('5');
     const [isReadyToRender, setIsReadyToRender] = useState(false);
-    const [dataToRender, setDataToRender] = useState();
+    const [dataToRender, setDataToRender] = useState(null);
     const [paginateLinks, setPaginateLinks] = useState([]);
 
     const operations = [{ 'name': 'Detalles', 'url': '/userDetail/' }];
-
     const headings = ['#', 'Nombre', 'DNI', 'Tipo de Usuario', 'Banneado', 'Estado', 'Operación'];
 
     useEffect(() => {
 
         const action = async () => {
-            await apiClient.get(`/getAllUsers/${registerPerPageValue}`).then((response) => {
+            const randomQueryParam = Math.random().toString(36).substring(7); // generando una cadena aleatoria para el parámetro de consulta
+            await apiClient.get(`/getAllUsers/${registerPerPageValue}?cache=${randomQueryParam}`).then((response) => {
                 setDataToRender(response.data.data);
                 setPaginateLinks(response.data.links);
                 setIsReadyToRender(true);
@@ -29,6 +29,7 @@ const UsersModule = ({ isLoggedIn, isAdmin, areUserStatusLoaded }) => {
 
         action();
     }, []);
+
 
     const handleRegisterPerPageChange = (e) => {
         setRegisterPerPageValue(e.target.value);
@@ -43,9 +44,7 @@ const UsersModule = ({ isLoggedIn, isAdmin, areUserStatusLoaded }) => {
                 console.log(error);
             });
         }
-
         action();
-
     }
 
     if (!areUserStatusLoaded) {
@@ -76,40 +75,50 @@ const UsersModule = ({ isLoggedIn, isAdmin, areUserStatusLoaded }) => {
             );
         }
 
-        return (
-            <div className='container-md container-style' style={{ color: 'black', padding: '2em' }}>
-                <h1 className="text-center" style={{ color: 'white' }} >Módulo de Usuarios</h1>
-                <div id="registerPerPage">
-                    <div className="form-floating">
-                        <select
-                            className="form-select"
-                            id="floatingSelect"
-                            aria-label="Floating label select example"
-                            value={registerPerPageValue}
-                            onChange={handleRegisterPerPageChange}
-                        >
-                            <option value="5">5</option>
-                            <option value="10">10</option>
-                            <option value="15">15</option>
-                            <option value="20">20</option>
-                            <option value="25">25</option>
-                            <option value="30">30</option>
-                        </select>
-                        <label htmlFor="floatingSelect">Registros por Página</label>
+        if (!isReadyToRender) {
+            return (
+                <div className='container-fluid' style={{ marginTop: '3em' }}>
+                    <div className="container d-flex justify-content-center">
+                        <Spinner animation="border" variant='light' />
                     </div>
                 </div>
-                <ResponsiveTable
-                    headings={headings}
-                    isReadyToRender={isReadyToRender}
-                    setIsReadyToRender={setIsReadyToRender}
-                    data={dataToRender}
-                    setData={setDataToRender}
-                    paginateLinks={paginateLinks}
-                    setLinks={setPaginateLinks}
-                    operations={operations}
-                />
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div className='container-md container-style' style={{ color: 'black', padding: '2em' }}>
+                    <h1 className="text-center" style={{ color: 'white' }} >Módulo de Usuarios</h1>
+                    <div id="registerPerPage">
+                        <div className="form-floating">
+                            <select
+                                className="form-select"
+                                id="floatingSelect"
+                                aria-label="Floating label select example"
+                                value={registerPerPageValue}
+                                onChange={handleRegisterPerPageChange}
+                            >
+                                <option value="5">5</option>
+                                <option value="10">10</option>
+                                <option value="15">15</option>
+                                <option value="20">20</option>
+                                <option value="25">25</option>
+                                <option value="30">30</option>
+                            </select>
+                            <label htmlFor="floatingSelect">Registros por Página</label>
+                        </div>
+                    </div>
+                    <ResponsiveTable
+                        headings={headings}
+                        isReadyToRender={isReadyToRender}
+                        setIsReadyToRender={setIsReadyToRender}
+                        data={dataToRender}
+                        setData={setDataToRender}
+                        paginateLinks={paginateLinks}
+                        setLinks={setPaginateLinks}
+                        operations={operations}
+                    />
+                </div>
+            );
+        }
     }
 };
 
