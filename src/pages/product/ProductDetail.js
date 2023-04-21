002cc3c1187a9d5e1643f82bdddf27637e79adba
iamchapita <alejandrom646@gmail.com>
2023-04-20 20:47:03 -0600

    Implementaciones: - Variables de rol de usuario. - Indicador de estado del producto. - Implementacion de boton de baneo de producto. - Refactorizacion de vista para estilos css.

--- src/pages/product/ProductDetail.js
+++ src/pages/product/ProductDetail.js
@@ -2,18 +2,24 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import apiClient from '../../utils/apiClient';
 import { Spinner } from 'react-bootstrap';
+import Button from "../../components/Button";
+
 import ShareButton from "../../components/shareButton";
 
-const ProductDetail = () => {
+const ProductDetail = ({ isAdmin, areUserStatusLoaded }) => {
 
     const [waitingResponse, setWaitingResponse] = useState(true);
     const [wasProductFound, setWasProductFound] = useState(null);
     const [responseMessage, setResponseMessage] = useState('');
     // La variable product almacena un objeto
     const [product, setProduct] = useState([]);
+    const [isBanned, setIsBanned] = useState(null);
+    const [isAvailable, setIsAvailable] = useState(null);
+    const [isPerformingAction, setIsPerformingAction] = useState(false);
     const [productImages, setProductImages] = useState([]);
     const [productExtensions, setProductExtensions] = useState([]);
     const [sellerDetails, setSellerDetails] = useState([]);
+
     const { id } = useParams();
 
     useEffect(() => {
@@ -21,6 +27,8 @@ const ProductDetail = () => {
             try {
                 const response = await apiClient.get(`/product/${id}`).then((productResponse) => {
                     setProduct(productResponse.data);
+                    setIsAvailable(Boolean(productResponse.data.isAvailable));
+                    setIsBanned(Boolean(productResponse.data.isBanned));
                     const sellerData = {
                         'id': productResponse.data.userIdFK,
                         'name': productResponse.data.userFirstName + ' ' + productResponse.data.userLastName,
@@ -77,79 +85,156 @@ const ProductDetail = () => {
         }
     }, [productImages]);
 
-    if (waitingResponse === true) {
+    const handleBanButtonAction = () => {
+        setIsPerformingAction(true);
+
+        const action = async () => {
+
+            await apiClient.post('/setProductIsBanned', {
+                id: id,
+                isBanned: !isBanned
+            }).then((response) => {
+                setIsBanned(!isBanned);
+                setIsPerformingAction(false);
+            }).catch((error) => {
+                console.log(error);
+            });
+        };
+        action();
+    };
+
+    if (areUserStatusLoaded === false) {
         return (
             <div className="container-sm d-flex justify-content-center">
                 <Spinner animation="border" />
             </div>
         );
-    }
 
-    if (wasProductFound === false) {
-        return (
-            <div className="container-sm">
-                <h1 className="text-center">{responseMessage}</h1>
-            </div>
-        );
-    }
+    } else {
 
-    if (waitingResponse === false) {
-        return (
-            <div className="d-flex justify-content-center align-items-center">
-            <div className="container-custom">
-                <div className="row center align-items-start">
-                    <div className="col-sm-12 col-md-12 col-lg-12">
+        if (waitingResponse === true) {
+            return (
+                <div className="container-sm d-flex justify-content-center">
+                    <Spinner animation="border" />
+                </div>
+            );
+        }
 
-                        <div id="carouselExampleIndicators" className="carousel slide" style={{ height: '60vh' }}>
+        if (wasProductFound === false) {
+            return (
+                <div className="container-sm">
+                    <h1 className="text-center">{responseMessage}</h1>
+                </div>
+            );
+        }
+
+        if (waitingResponse === false) {
+            return (
+                <div className={`container-md container-style ${isAdmin ? (isBanned ? 'isBanned' : !isAvailable ? 'isAvailable' : '') : (null)}`} style={{ color: "white" }}>
+                    <div className="row p-3">
+
+                        <div id="carouselExampleIndicators" className="carousel carousel-dark slide" style={{ height: '60vh' }}>
                             <div className="carousel-indicators">
                                 {
                                     productImages.map((image, index) => (
-                                        <button key={index} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={index} className={index === 0 ? 'active' : ''} aria-label={`Slide ${index}`}></button>
+                                        <button
+                                            key={index}
+                                            type="button"
+                                            data-bs-target="#carouselExampleIndicators"
+                                            data-bs-slide-to={index}
+                                            className={index === 0 ? 'active' : ''}
+                                            aria-label={`Slide ${index}`}
+                                        >
+                                        </button>
                                     ))
                                 }
                             </div>
                             <div className="carousel-inner">
                                 {
                                     productImages.map((image, index) => (
-                                        <div key={index} className={`carousel-item ${index == 0 ? 'active' : ''}`}>
-                                            <img src={`data:image/${productExtensions[index]};base64,${image.base64Image}`} className="img-fluids" alt="..." style={{ objectFit: 'cover' }} />
+                                        <div
+                                            key={index}
+                                            className={`carousel-item ${index == 0 ? 'active' : ''}`}
+                                        >
+                                            <img
+                                                src={`data:image/${productExtensions[index]};base64,${image.base64Image}`}
+                                                style={{ maxHeight: '60vh' }}
+                                            />
                                         </div>
                                     ))
                                 }
                             </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev"
+                                style={{ marginLeft: '3em' }}>
                                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span className="visually-hidden">Previous</span>
                             </button>
-                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next"
+                                style={{ marginRight: '3em' }}>
                                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span className="visually-hidden">Next</span>
                             </button>
                         </div>
-
-                        <div className="col-sm-10 col-md-10 col-lg-12 productDetailText">
+                    </div>
+                    <div className="row p-3">
+                        <div className="col-sm-12 col-md-12 col-lg-12 productDetailText">
                             {
                                 <div className="product-description">
                                     <h1>{product.name}</h1>
-                                    <h4>Vendedor: <a href={`/userProfile/${sellerDetails.id}`}>{sellerDetails.name}</a></h4> 
-                                    <h4>Precio: L. {product.price.toLocaleString()}</h4>
+                                    <h4>Vendedor: <a href={`/userProfile/${sellerDetails.id}`}>{sellerDetails.name}</a></h4>
+                                    <h4>Precio: L {product.price.toLocaleString()}</h4>
                                     <h4>Dirección: {sellerDetails.direction}</h4>
                                     <h4>Estado del Producto: {product.status}</h4>
                                 </div>
                             }
                             {
-                            /* <button type="button" className="btn btn-secondary">Agregar a Lista de Deseos</button> */
-                            
-                            //BOTON PARA COMPARTIR 
-                            <ShareButton></ShareButton>
+                                isAdmin ? (
+                                    isPerformingAction ? (
+                                        <div>
+                                            <hr></hr>
+                                            <Button
+                                                type={'button'}
+                                                buttonClass={'danger'}
+                                                tooltipText={'Espera'}
+                                                diabled={!isPerformingAction}
+                                                fieldLabel={
+                                                    <Spinner
+                                                        animation="border"
+                                                        variant="light"
+                                                        size="sm"
+                                                    />
+                                                }
+                                            />
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            <hr></hr>
+                                            <Button
+                                                fieldLabel={
+                                                    isBanned ?
+                                                        'Desbannear' :
+                                                        'Bannear'
+                                                }
+                                                type={'button'}
+                                                buttonClass={'danger'}
+                                                tooltipText={
+                                                    isBanned ?
+                                                        'Desbanee el producto.' :
+                                                        'Banee el producto.'
+                                                }
+                                                onClick={handleBanButtonAction}
+                                            />
+                                        </div>
+                                    )
+                                ) : (
+                                    <ShareButton />
+                                )
                             }
                         </div>
-                        
                     </div>
                 </div>
-            </div>
-            </div>
-        )
+            )
+        }
     }
 }
 
