0ad1514838e0e44c60b2ea5371452bb57932972b
iamchapita <alejandrom646@gmail.com>
2023-04-25 22:30:05 -0600

    Implementacion de variable para definir el tipo de alerta

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -1,11 +1,11 @@
 
 import React from "react";
 
-const Alert = ({ text, showAlert, setShowAlert }) => {
+const Alert = ({ text, showAlert, setShowAlert, type = 'danger' }) => {
 
     if (showAlert) {
         return (
-            <div className="alert alert-danger alert-dismissible fade show" role="alert">
+            <div className={`alert alert-${type} alert-dismissible fade show`} role="alert">
                 <strong>Ups!</strong><p dangerouslySetInnerHTML={{__html: text}}></p>
                 <button type="button" className="btn-close" onClick={() => setShowAlert(!showAlert)} data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
