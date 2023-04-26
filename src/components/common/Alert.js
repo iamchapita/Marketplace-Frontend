3b68eab3dac23c88471e0b7eca42af440e9359d0
iamchapita <alejandrom646@gmail.com>
2023-04-25 22:44:30 -0600

    Implementacion de parametrizacion de texto entre etiquetas Strong

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -1,12 +1,12 @@
 
 import React from "react";
 
-const Alert = ({ text, showAlert, setShowAlert, type = 'danger' }) => {
+const Alert = ({ text, showAlert, setShowAlert, type = 'danger', strongText = 'Ups!' }) => {
 
     if (showAlert) {
         return (
             <div className={`alert alert-${type} alert-dismissible fade show`} role="alert">
-                <strong>Ups!</strong><p dangerouslySetInnerHTML={{__html: text}}></p>
+                <strong>{strongText}</strong><p dangerouslySetInnerHTML={{ __html: text }}></p>
                 <button type="button" className="btn-close" onClick={() => setShowAlert(!showAlert)} data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         );
