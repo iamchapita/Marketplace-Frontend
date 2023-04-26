838592eb14bf0abc946c1c48d0ff50ba96994ab1
iamchapita <alejandrom646@gmail.com>
2023-04-25 22:44:08 -0600

    Implementacion de atributo deshabilitado

--- src/components/InputFile.js
+++ src/components/InputFile.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const InputFile = ({ type, fieldLabel, fieldName, placeholder, inputValue, onChangeHandler, accept = ['*/*'], required = true, isValid = true }) => {
+const InputFile = ({ type, fieldLabel, fieldName, placeholder, inputValue, onChangeHandler, accept = ['*/*'], required = true, disabled = false, isValid = true }) => {
 
     return (
         <div className="input-group mb-3">
             <label className="input-group-text" htmlFor="inputGroupFile01">{fieldLabel}</label>
-            <input type={type} className={`form-control ${isValid ? '' : "invalid"}`} id="formFileMultiple" placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} autoComplete="off" required={required} multiple onChange={onChangeHandler} accept={accept.length > 0 ? accept.map((value) => value) : accept[0]} />
+            <input type={type} className={`form-control ${isValid ? '' : "invalid"}`} id="formFileMultiple" placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} autoComplete="off" required={required} multiple onChange={onChangeHandler} disabled={disabled} accept={accept.length > 0 ? accept.map((value) => value) : accept[0]} />
         </div>
     );
 
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,22 +1,14 @@
 import React from "react";
 
-const TextArea = ({ fieldLabel, fieldName, placeholder, inputValue, onChangeHandler, required = true, isValid = true }) => {
+const TextArea = ({ fieldLabel, fieldName, placeholder, inputValue, onChangeHandler, required = true, disabled = false, isValid = true }) => {
 
-    if (required) {
-        return (
-            <div className="input-group mb-3">
-                <span className="input-group-text" id="basic-addon1">{fieldLabel}</span>
-                <textarea rows="3" className={`form-control ${isValid ? '' : "invalid"}`} placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} value={inputValue} autoComplete="off" required onChange={onChangeHandler}></textarea>
-            </div>
-        );
-    } else {
-        return (
-            <div className="input-group mb-3">
-                <span className="input-group-text" id="basic-addon1">{fieldLabel}</span>
-                <textarea rows="3" className={`form-control ${isValid ? '' : "invalid"}`} placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} value={inputValue} autoComplete="off" onChange={onChangeHandler}></textarea>
-            </div>
-        );
-    }
+    return (
+        <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">{fieldLabel}</span>
+            <textarea rows="3" className={`form-control ${isValid ? '' : "invalid"}`} placeholder={placeholder} aria-label={fieldName} name={fieldName} aria-describedby={fieldName} value={inputValue} autoComplete="off" required={required} onChange={onChangeHandler} disabled={disabled}></textarea>
+        </div>
+    );
+
 }
 
 export default TextArea;
