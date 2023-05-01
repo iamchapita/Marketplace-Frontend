58d45d61e341f8e63476797fca244b352be3f862
iamchapita <alejandrom646@gmail.com>
2023-04-30 20:11:40 -0600

    Refactorización

--- src/components/HomeProductCard.js
+++ src/components/HomeProductCard.js
@@ -68,78 +68,49 @@ function HomeProductCard({ id, userId, name, price, urlDetalles, path, idSeller,
         }
     }
 
-    if (isWhisListStatusInclude) {
-        return (
-            <div className="container card-publication" key={id}>
-                <div className="card home-card">
-                    <div className="account-container">
-                        <div className="count-bar">
-                            <i className="material-icons icon-perfil" >person</i>
-                            <a className="link-perfil" href={`/userProfile/${idSeller}`} >{nameSeller}</a>
-                        </div>
+    return (
+        <div className="container card-publication" key={id}>
+            <div className="card home-card">
+                <div className="account-container">
+                    <div className="count-bar">
+                        <i className="material-icons icon-perfil" >person</i>
+                        <a className="link-perfil" href={`/userProfile/${idSeller}`} >{nameSeller}</a>
                     </div>
-                    <div className="col-4">
-                        {
-                            !isReadyToRender ? (<div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                                <div className="d-flex align-items-center justify-content-center">
-                                    <Spinner animation="border" variant="light" />
-                                </div>
-                            </div>) : (productImage.map((image, index) => (
-                                <div key={index} className="img-card" >
-                                    <img src={`data:image/${productExtension[index]};base64,${image.base64Image}`} className="img-fluid" />
-                                </div>
-                            )))
-                        }
-                    </div>
-                    <div className="card-body home-card-body" key={id} >
-                        <h5 className="card-title">{name}</h5>
-                        <h6 className="card-text" >L {price.toLocaleString()}</h6>
-                        <a className="card-link" href={urlDetalles}>Detalles</a>
-                        <div className="check-container">
-
-                            <label id={`labelCheck${id}`} htmlFor={`check${id}`}>
-                                <i className={`material-icons ${favoriteClass ? 'icon-favorite-active ' : 'icon-favorite'}`} >favorite</i>
-                            </label>
-                            <input className="check-invisible" onClick={() => favorite(id)} type="checkbox" id={`check${id}`} />
-                        </div>
-                    </div>
+                </div>
+                <div className="col-4">
+                    {
+                        !isReadyToRender ? (<div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <Spinner animation="border" variant="light" />
+                            </div>
+                        </div>) : (productImage.map((image, index) => (
+                            <div key={index} className="img-card" >
+                                <img src={`data:image/${productExtension[index]};base64,${image.base64Image}`} className="img-fluid" />
+                            </div>
+                        )))
+                    }
+                </div>
+                <div className="card-body home-card-body" key={id} >
+                    <h5 className="card-title">{name}</h5>
+                    <h6 className="card-text" >L {price.toLocaleString()}</h6>
+                    <a className="card-link" href={urlDetalles}>Detalles</a>
+                    {
+                        isWhisListStatusInclude ? (
+                            <div className="check-container">
+                                <label id={`labelCheck${id}`} htmlFor={`check${id}`}>
+                                    <i className={`material-icons ${favoriteClass ? 'icon-favorite-active ' : 'icon-favorite'}`} >favorite</i>
+                                </label>
+                                <input className="check-invisible" onClick={() => favorite(id)} type="checkbox" id={`check${id}`} />
+                            </div>
+                        ) : (
+                            null
+                        )
+                    }
 
                 </div>
             </div>
-        );
-    } else {
-        return (
-            <div className="container card-publication" key={id}>
-                <div className="card home-card">
-                    <div className="account-container">
-                        <div className="count-bar">
-                            <i className="material-icons icon-perfil" >person</i>
-                            <a className="link-perfil" >{nameSeller}</a>
-                        </div>
-                    </div>
-                    <div className="col-4">
-                        {
-                            !isReadyToRender ? (<div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                                <div className="d-flex align-items-center justify-content-center">
-                                    <Spinner animation="border" variant="light" />
-                                </div>
-                            </div>) : (productImage.map((image, index) => (
-                                <div key={index} className="img-card" >
-                                    <img src={`data:image/${productExtension[index]};base64,${image.base64Image}`} className="img-fluid" />
-                                </div>
-                            )))
-                        }
-                    </div>
-                    <div className="card-body home-card-body" key={id} >
-                        <h5 className="card-title">{name}</h5>
-                        <h6 className="card-text" >L {price.toLocaleString()}</h6>
-                        <a className="card-link" href={urlDetalles}>Detalles</a>
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default HomeProductCard;
