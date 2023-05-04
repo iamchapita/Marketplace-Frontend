d91497af714115d9fee79657a714dc73f7d2b68d
Ajzunigag <104332622+Ajzunigag@users.noreply.github.com>
2023-05-03 23:00:26 -0600

    Implementación
    grafico de productos vendidos

--- src/pages/admin/modules/StatisticsModule/StatisticsModule.js
+++ src/pages/admin/modules/StatisticsModule/StatisticsModule.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 
 import CustomizableAlert from "../../../../components/CustomizableAlert";
 import apiClient from "../../../../utils/apiClient";
-import { YAxis, Line, CartesianGrid, ComposedChart, BarChart, XAxis, Tooltip, Legend, Bar, ResponsiveContainer } from 'recharts';
+import { Area, YAxis, Line, CartesianGrid, ComposedChart, BarChart, XAxis, Tooltip, Legend, Bar, ResponsiveContainer } from 'recharts';
 
 
 const StatisticsModule = ({ isLoggedIn, isAdmin, areUserStatusLoaded }) => {
     const [products, setProducts] = useState([]);
     const [gradit, setGrafic] = useState([]);
     const [showChart, setShowChart] = useState(false);
+
+    const [sales, setSales] = useState([]);
+
+    const [showChart1, setShowChart1] = useState(false);
 
     useEffect(() => {
         const getProducts = async () => {
@@ -20,6 +24,18 @@ const StatisticsModule = ({ isLoggedIn, isAdmin, areUserStatusLoaded }) => {
             }
         }
         getProducts();
+    }, []);
+
+    useEffect(() => {
+        const getSales = async () => {
+            try {
+                const response = await apiClient.get('/getProductoVendidos');
+                setSales(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getSales();
     }, []);
 
 
@@ -47,31 +63,70 @@ const StatisticsModule = ({ isLoggedIn, isAdmin, areUserStatusLoaded }) => {
             setShowChart(!showChart);
         };
 
+        const handleButtonClickSale = async () => {
+            const res = await apiClient.get("/getProductoVendidos");
+            setSales(res.data);
+            setShowChart1(!showChart1);
+        };
+
+
 
         return (
             <div>
-                <button onClick={handleButtonClick}>Mostrar y Ocultar gráfico</button>
+                <div>
+                    <button onClick={handleButtonClick}>Mostrar y Ocultar Gráfico Usuarios</button>
 
-                {/* Graficas de productos Por departamento */}
-                {showChart && (
-                    <div>
-                        {/* Graficas de productos Insertados */}
-                        <h2>Grafica de Productos Registrados</h2>
-                        <BarChart
-                            data={products}
-                            width={500}
-                            height={300}>
-                            <XAxis dataKey="nombre_mes" stroke="#8884d8" />
-                            <YAxis />
-                            <Tooltip />
-                            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                            <Bar dataKey="dia" fill="#8884d8" barSize={30} />
-                            <Bar dataKey="Nombre" fill="#8884d8" barSize={30} />
-                            <Bar dataKey="anio" fill="#8884d8" barSize={30} />
-                        </BarChart>
+                    {/* Graficas de productos Por departamento */}
+                    {showChart && (
+                        <div className="bg-white">
+                            {/* Graficas de productos Insertados */}
+                            <h2>Gráfica de Productos Registrados</h2>
+                            <BarChart
+                                data={products}
+                                width={600}
+                                height={300}>
+                                <XAxis dataKey="nombre_mes" stroke="#8884d8" />
+                                <YAxis />
+                                <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+                                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                                <Bar dataKey="Día" fill="#8884d8" barSize={30} />
+                                <Bar dataKey="Nombre" fill="#8884d8" barSize={30} />
+                                <Bar dataKey="Año" fill="#8884d8" barSize={30} />
+                                <Line type="monotone" dataKey="Día" stroke="#ff7300" />
 
-                    </div>
-                )}
+                            </BarChart>
+                           
+
+                        </div>
+                    )}
+
+                </div>
+
+                {/* Graficas de productos vendidos por departamentos  */}
+                <div>
+                    <button onClick={handleButtonClickSale}>Mostrar y Ocultar Gráfico Ventas</button>
+
+                    {/* Graficas de productos Por departamento */}
+                    {showChart1 && (
+                        <div className="bg-white">
+                            {/* Graficas de productos Insertados */}
+                            <h2>Gráfica de Productos Vendidos</h2>
+                            <BarChart width={600} height={300} data={sales}>
+                                <XAxis dataKey="departamento" stroke="#1E90FF" />
+                                
+                                <YAxis />
+                                <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+                                
+                                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                                <Bar dataKey="Nombre"  fill="#800080" barSize={30} />
+                                <Bar dataKey="Total_ProductosVendidos"  fill="#1E90FF" barSize={30} />
+                               
+                            </BarChart>
+                        </div>
+                    )}
+
+                </div>
+
 
             </div>
 
